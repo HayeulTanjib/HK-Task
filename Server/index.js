50872fe039c5e516b9c58e24d994db97f25964ce
unknown <tanjib.cse@gmail.com>
2023-01-17 01:42:02 +0600

    added sectors GET API

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,92 @@
-const cors = require('cors');
-const express = require('express');
-const app = express();
-const client = require('./mongo-config');
-const port = process.env.PORT || 5000;
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const uri = require('./mongo-config');
+const port = 5000;
 
 //middleware
-app.use(cors())              
-app.use(express.json())
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+
+//DB connection
+const connectDB = async() => {
+    try {
+        await mongoose.connect(uri);
+        console.log("DB Connected");
+    } catch (error) {
+        console.log(error.message);
+        process.emit(1);
+    }
+}
+
+//Server Listenting Port
+server.listen(port, async() => {
+    console.log(`Server is running: ${port}`);
+    await connectDB();
+});
 
 
-
-app.get('/', (req, res) => {
-    res.send("Server Ready!");
+//schema
+const sectorsSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: [true, "sector category is required"]
+    },
+    subCategory: {
+        type: Array,
+        required: [true, "sector subcategories are required"]
+    }
 })
 
-app.listen(port, () => {
-    console.log("Listening on: ", port);
+const userDataSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "name is required"]
+    },
+    sectors: {
+        type: Array,
+        required: [true, "sectors are required"]
+    },
+    isAgree: {
+        type: Boolean,
+        required: [true, "agreement is required"]
+    }
+
+})
+
+//model
+const Sector = mongoose.model("sectors", sectorsSchema);
+const User = mongoose.model("userdatas", userDataSchema)
+
+server.get("/sectors", async(req, res) => {
+    try {
+        const sectors = await Sector.find();
+        sectors ? res.status(200).send({
+            success: true,
+            message: "return all sectors",
+            data: sectors
+        }):
+        res.status(404).send({
+            success: false,
+            message: "sectors not found"
+        });
+
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+})
+
+
+server.post('/userdata', async(req, res) => {
+    try {
+        const userData = ({name: "John", sectors: ['apple', 'orange'], isAgree: true});
+        const newUserData = new User(userData);
+        const value = await newUserData.save();
+        res.status(201).send(value);
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
 })
 
 
--- Server/mongo-config.js
+++ Server/mongo-config.js
@@ -1,6 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@hk-team.szvfbh9.mongodb.net/?retryWrites=true&w=majority`;
-module.exports =  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+module.exports = uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@hk-team.szvfbh9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
